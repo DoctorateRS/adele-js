747900169f337249310a071e5a49d61df53fb95f
Rh4096 <rhelvetican@gmail.com>
2025-02-02 21:32:27 +0700

    raposa

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import settings from "../config/config.json" with { type: "json" };
+
 import { Application } from "oak";
 const app = new Application();
+
 app.listen({
-  hostname: settings.server.host,
-  port: settings.server.port,
+    hostname: settings.server.host,
+    port: settings.server.port,
 });
