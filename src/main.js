fb7e1f6e1d6fca276ed0239b2864ca58aaa65f26
Rh4096 <rhelvetican@gmail.com>
2025-02-05 23:17:44 +0700

    reform

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
+import settings from "../config/config.json" with { type: "json" };
+import { assetbundle } from "./adele/assetbundle.js";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route("/assetbundle/official/Android/assets", assetbundle);
 
-Deno.serve(app.fetch);
+Deno.serve({ hostname: settings.server.host, port: settings.server.port }, app.fetch);
