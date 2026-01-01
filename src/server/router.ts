fa824fb5ab10398a1b7c98e91c9a24edfe9c8a44
Rh4096 <rhelvetican@gmail.com>
2026-01-01 22:44:58 +0700

    "ReEntrant"

--- src/server/router.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from "oak";
-export const masterRouter = new Router();
-import { accountRouter } from "./account.ts";
-masterRouter.use("/account", accountRouter.routes(), accountRouter.allowedMethods());
